015b76f9e9c627c89e46da812c8601af91e20789
utkarshuday <120191592+utkarshuday@users.noreply.github.com>
2024-11-27 16:44:29 +0530

    update

--- sentiments.py
+++ sentiments.py
@@ -7,9 +7,11 @@ headers = {"Authorization": "Bearer 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"}
 
 
 def sentiment_analysis(message):
-  print(os.getenv("HUGGING_FACE_TOKEN"))
-  payload = {"inputs": message.text}
-  response = requests.post(API_URL, headers=headers, json=payload)
-  json = response.json()
-  tone = json[0][0]['label']
-  return f"This text has a tone of *{tone}*"
+  try:
+    payload = {"inputs": message.text}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    json = response.json()
+    tone = json[0][0]['label']
+    return f"This text has a tone of *{tone}*"
+  except:
+    return "Some error occurred"
